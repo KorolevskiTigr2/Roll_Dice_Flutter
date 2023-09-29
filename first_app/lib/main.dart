373f20edfe46a5507daf8b544a97dba1887f2c5b
Alexander Medvedev <mega.medved87@gmail.com>
2023-09-29 18:20:51 -0500

    Style: text('Hello world')

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -9,12 +9,17 @@ void main() {
             gradient: LinearGradient(
               colors: [
                 Color.fromARGB(255, 25, 14, 16),
-                Color.fromARGB(253, 25, 14, 16),
+                Color.fromARGB(253, 25, 14, 16)
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
-          child: const Center(
-            child: Text('Hello world'),
+          child: const Center(    
+            child: Text('Hello world', style: TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+            )),
           ),
         ),
       ),
